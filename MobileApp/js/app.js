8f7299ca2eb015fd15b4d21f88a1dfa0f2a4d361
bobbychaz <rob@robvolk.com>
2013-06-28 17:11:30 -0600

    Static LiveVibes

---
+++ MobileApp/js/app.js
@@ -0,0 +1,35 @@
+$(document).bind("mobileinit", function(){
+  $.mobile.defaultPageTransition = 'slide';
+});
+
+$(function() {
+  // initialize client with app credentials
+  SC.initialize({
+    client_id: 'c96c39c3a9e0e0cb272b834ee9b8915d',
+    redirect_uri: 'file:///C:/Dev/LiveVibes/MobileApp/index.html#events'
+  });
+
+  $('.connect-soundcloud').click(function() {
+    // initiate auth popup
+    SC.connect(function() {
+      SC.get('/me', function(me) {
+        alert('Hello, ' + me.username);
+      });
+    });
+    return false;
+  })
+
+});
+
+
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return ['red', 'yellow', 'blue'];
+  }
+});
